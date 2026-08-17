b1837492a71e763b6b46b6941cb68d07a95e9fee
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-17 23:39:09 +0330

    Update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,24 @@
+from database import create_tables
+from movie_service import add_movie, get_movies
 from omdb_api import search_movie
 
-print(search_movie(""))
-print(search_movie("asdkjaskjdaksjd"))
-print(search_movie("Interstellar"))
+
+create_tables()
+
+movie = search_movie("Interstellar")
+
+if movie:
+
+    add_movie(
+        imdb_id=movie["imdb_id"],
+        title=movie["title"],
+        release_date=movie["release_date"],
+        runtime=movie["runtime"],
+        rating=movie["rating"],
+        overview=movie["overview"]
+    )
+
+movies = get_movies()
+
+for movie in movies:
+    print(movie)
