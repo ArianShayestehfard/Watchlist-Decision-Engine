b557ee9cc27ab35abf9116c33aea02e22947faa4
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-19 15:40:04 +0330

    Update

--- app/main.py
+++ app/main.py
@@ -1,24 +1,8 @@
-from database import create_tables
-from movie_service import add_movie, get_movies
-from omdb_api import search_movie
+from analytics import watched_stats, top_rated
 
+stats = watched_stats()
+print(f"Watched: {stats['count']} movies, average rating: {stats['average_rating']}")
 
-create_tables()
-
-movie = search_movie("Interstellar")
-
-if movie:
-
-    add_movie(
-        imdb_id=movie["imdb_id"],
-        title=movie["title"],
-        release_date=movie["release_date"],
-        runtime=movie["runtime"],
-        rating=movie["rating"],
-        overview=movie["overview"]
-    )
-
-movies = get_movies()
-
-for movie in movies:
+print("--- Top rated in your list ---")
+for movie in top_rated(limit=3):
     print(movie)
