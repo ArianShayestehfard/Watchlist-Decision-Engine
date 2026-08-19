e46f11df9e621b185bdb64b03a9985c61b394fc6
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-19 21:11:41 +0330

    Update

--- app/recommendation.py
+++ app/recommendation.py
@@ -1,21 +1,14 @@
 from movie_service import get_movies_by_status
 
-
 def recommend_movie():
     candidates = get_movies_by_status("want_to_watch")
-
     rated_candidates = [movie for movie in candidates if movie[5] is not None]
-
     if not rated_candidates:
         return None
-
     return max(rated_candidates, key=lambda m: m[5])
-
 
 def recommend_top_n(n=3):
     candidates = get_movies_by_status("want_to_watch")
-
     rated_candidates = [movie for movie in candidates if movie[5] is not None]
     sorted_candidates = sorted(rated_candidates, key=lambda m: m[5], reverse=True)
-
     return sorted_candidates[:n]
