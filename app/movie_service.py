312ea896645f88afcd446715a42043a84b27db69
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-19 21:17:41 +0330

    Update

--- app/movie_service.py
+++ app/movie_service.py
@@ -184,11 +184,3 @@ def find_movies_by_title(title):
     movies = cursor.fetchall()
     connection.close()
     return movies
-
-def get_movie_by_exact_title(title):
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT id, title, imdb_id FROM movies WHERE LOWER(title) = ?", (title.lower(),))
-    movie = cursor.fetchone()
-    connection.close()
-    return movie
