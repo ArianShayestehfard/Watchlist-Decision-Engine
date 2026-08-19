60e4aed65997160ff1821262ebf4cd1831046bcf
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-19 20:58:01 +0330

    validators Update

--- app/validators.py
+++ app/validators.py
@@ -1,3 +1,6 @@
+from constants import VALID_STATUSES
+from movie_service import find_movies_by_title, get_movie_by_exact_title
+
 def get_valid_status(prompt):
     while True:
         status = input(prompt).strip()
