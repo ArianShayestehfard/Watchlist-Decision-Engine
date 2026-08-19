dc1f72e2f0d6baa215d91dc5fe335453a0cc3ea6
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-20 00:26:15 +0330

    Update

--- app/add_genres.py
+++ app/add_genres.py
@@ -1,3 +1,6 @@
+from movie_service import get_movies, get_movie_by_id
+from omdb_api import search_movie
+from database import get_connection
 
 def add_genre(genre_name):
     conn = get_connection()
