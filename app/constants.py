325b4d2e9f0542483349b7ce674f84ecebe9b324
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-08-19 20:58:56 +0330

    constants Update

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+VALID_STATUSES = ("want_to_watch", "watching", "watched")
+MENU_OPTIONS = {
+    1: "Search Movie",
+    2: "View Watchlist",
+    3: "Add Movie to Watchlist",
+    4: "Change Movie Status",
+    5: "Rate Movie",
+    6: "Recommend Movie",
+    7: "View Statistics",
+    8: "Exit"
+}
